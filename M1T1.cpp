7923bdf1018c46eef54307cc9ffd0b26bc9659b4
cyla-lacuniak <cyla.howard12@yahoo.com>
2026-02-02 23:48:15 +0000

    finished m1t1

--- M1T1.cpp
+++ M1T1.cpp
@@ -10,5 +10,11 @@ cyla
 using namespace std;
 
 int main() {
+    //Greet the user
+    cout << "Hello, user!" << endl; //this is end "L" for endline not end "1"
+    cout << "What's your name" << endl;
+    string name;
+    cin >> name;
+    cout << "Nice to meet you, " << name << endl;
     return 0;
 }
