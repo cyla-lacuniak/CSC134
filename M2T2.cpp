86b92eff5a9413c153bc132d3415f5ed61d7e41c
cyla-lacuniak <cyla.howard12@yahoo.com>
2026-02-16 18:31:41 +0000

    set variables

--- M2T2.cpp
+++ M2T2.cpp
@@ -11,6 +11,25 @@ Sales tax will be 8% for this project
 using namespace std;
 
 int main() {
+    // for this project we are making a receipt printer for a restaurant 
+    // declare all of my variables for the project
+    string meal_name = "Taco Bowl"; 
+    double meal_price = 5.99; 
+    double tax_rate = 0.08;         // this is 8% - 8% is 8/100 ("per cent")
+    double tip_amount = 
+    double tax_amount =             // $ of the actual tax charged
+    double total_price =            // meal + tip + tax
+    // get user input
+
+    // do the calculation
+
+    // present the output
+
+
+
+
+
+
     return 0; // no erros
 
 
