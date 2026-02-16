2d8953483741f424a6f7ceefe62cf8498f2579bb
cyla-lacuniak <cyla.howard12@yahoo.com>
2026-02-16 18:24:54 +0000

    started m2t2

---
+++ M2T2.cpp
@@ -0,0 +1,24 @@
+/*
+CSC 134
+M2T2 
+Cyla Lacuniak
+16 February 2026
+
+Sales tax will be 8% for this project
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0; // no erros
+
+
+
+
+
+
+
+
+
+}
